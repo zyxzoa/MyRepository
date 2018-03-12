ef70fecba2bb542e11627d759523a412fd5c3a9e
zyxzoa <zyxzoa@163.com>
2018-03-12 22:55:36 +0800

    pytestx

---
+++ tx.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-01-03 21:32:05
+# @Author  : zyx (zyxzoa@163.com)
+# @Link    : https://www.python.org/
+# @Version : 1.0
+
+import os
+from math import sqrt
+# _ = 'worl'
+# print("hello" + _)
+print(sqrt(3)*sqrt(3))
+print(sqrt(9)*sqrt(1)==3)
